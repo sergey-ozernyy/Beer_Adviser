2b5976522d5d03e53f9177641f9bd0631f8d8d01
sergey-ozernyy <sergey.ozernyy@mail.ru>
2020-02-11 20:48:59 +0300

    Initial commit

---
+++ app/src/main/java/com/hfad/beeradviser/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.hfad.beeradviser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        fun onClickFindBeer(view V) {}
+    }
+}
