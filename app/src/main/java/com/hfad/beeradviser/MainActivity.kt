75d3daf5c59e44d14463c1243c09a9995732c0a0
sergey-ozernyy <sergey.ozernyy@mail.ru>
2020-02-17 23:06:09 +0300

    Все новое, по документации девелоперс.андроид

--- app/src/main/java/com/hfad/beeradviser/MainActivity.kt
+++ app/src/main/java/com/hfad/beeradviser/MainActivity.kt
@@ -1,20 +1,57 @@
 package com.hfad.beeradviser
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setOnClickListener.find_beer{onClickFindBeer()}
-        
-    }
-    fun onClickFindBeer() {
-        val color:Spinner = findViewById(R.id.color)
-        val findColor:String = color.getItemSelected().toString
-        brends.text = resources.getText(R.strings.findColor)
+
+
+        val spinner: Spinner = findViewById(R.id.planets_spinner)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.planets_arrey,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+            spinner.adapter = adapter
+        }
+
+        class SpinnerActivity : Activity(), AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+
+            }
+        }
+
+
+        spinner.onItemSelectedListener = this
+
+        brands.text = spinner.onItemClickListener.toString()
+
     }
 }
